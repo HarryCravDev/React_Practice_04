27c3b78a1f48756254fd2155b9f6e5d42d1ce825
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-02 18:03:43 +0100

    Added clickColor, colorHeader methods.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
 import React from "react";
 import "./App.css";
-import ColorContainer from "./Components/Colour_Container/ColourContainer";
+import ColourContainer from "./Components/Colour_Container/ColourContainer";
+import Header from "./Components/Header/Header";
 
 class App extends React.Component {
   state = {
     colors: [],
-    selectedColour: Math.floor(Math.random() * 6) + 1,
+    selectedColor: Math.floor(Math.random() * 6),
     difficulty: {
       easy: 3,
       hard: 6,
     },
+    headerColor: "RGB(0, 0, 0)",
   };
 
   componentDidMount() {
     // Get Colors
     let colors = [];
-    for (let i = 0; i < this.state.number; i++) {
+    for (let i = 0; i < this.state.difficulty.hard; i++) {
       let r = Math.floor(Math.random() * 256);
       let g = Math.floor(Math.random() * 256);
       let b = Math.floor(Math.random() * 256);
       colors.push({
         id: i,
-        colour: `rgb(${r}, ${g}, ${b})`,
+        color: `rgb(${r}, ${g}, ${b})`,
       });
     }
+    console.log("Mount");
     // Update state
     this.setState({ colors: colors });
   }
 
-  getColour = () => {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+  // Update Header with winning color
+  colorHeader = (id) => {
+    const colorArray = this.state.colors.filter((color) => color.id === id);
+    const headerColor = colorArray[0];
+    this.setState({ headerColor: headerColor.color });
+  };
 
-    // colors.push({ colour: "rgb(" + r + ", " + g + ", " + b + ")" });
+  // User Click color
+  clickColor = (color) => {
+    console.log(color);
+    // Check if answer is correct
+    if (color === this.state.selectedColor) {
+      alert("Winner");
+      this.colorHeader(color);
+    }
+
+    // Filter colors
+    const updateColors = this.state.colors.filter((item) => item.id !== color);
+
+    // Update State
+    this.setState({ colors: updateColors });
   };
 
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
-        <ColorContainer colours={this.state.colors} />
+        <Header color={this.state.headerColor} />
+        <ColourContainer
+          clickColor={this.clickColor}
+          colors={this.state.colors}
+        />
       </div>
     );
   }
