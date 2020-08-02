f9f334c288a03ab342973638352c523dda725f00
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-02 15:04:07 +0100

    Project setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ColorContainer from "./Components/Colour_Container/ColourContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    colors: [],
+    selectedColour: Math.floor(Math.random() * 6) + 1,
+    difficulty: {
+      easy: 3,
+      hard: 6,
+    },
+  };
+
+  componentDidMount() {
+    // Get Colors
+    let colors = [];
+    for (let i = 0; i < this.state.number; i++) {
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
+      colors.push({
+        id: i,
+        colour: `rgb(${r}, ${g}, ${b})`,
+      });
+    }
+    // Update state
+    this.setState({ colors: colors });
+  }
+
+  getColour = () => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+
+    // colors.push({ colour: "rgb(" + r + ", " + g + ", " + b + ")" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <ColorContainer colours={this.state.colors} />
+      </div>
+    );
+  }
 }
 
 export default App;
