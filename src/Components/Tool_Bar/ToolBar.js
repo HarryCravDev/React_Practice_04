6b87627bc14f3f8cd4bc3bbd6c8f019939fe14f5
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-03 14:10:41 +0100

    Adding Toolbar component and stylesheet.

---
+++ src/Components/Tool_Bar/ToolBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ToolBar.styles.css";
+
+class ToolBar extends React.Component {
+  render() {
+    return (
+      <div className="toolbar-container">
+        <h1>Gello There</h1>
+      </div>
+    );
+  }
+}
+
+export default ToolBar;
