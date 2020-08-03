90bf13527f2cea14714d87463c38f5ad58f8b3f1
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-03 14:04:36 +0100

    Added dynamic RGB value in Header.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Header.styles.css";
 
-const Header = ({ color }) => {
+const Header = ({ color, rgbValue }) => {
   return (
     <div className="header-container" style={{ backgroundColor: color }}>
       <h1 className="ui header">RGB Colour Guessing Game</h1>
-      <h1 className="ui header">RGB(0, 0, 0)</h1>
+      <h1 className="ui header">{rgbValue}</h1>
     </div>
   );
 };
