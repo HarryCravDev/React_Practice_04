15409a41986813db1436722438d97e39954ca6a5
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-09 18:36:01 +0100

    Added styles to Toolbar elements.

--- src/Components/Tool_Bar/ToolBar.js
+++ src/Components/Tool_Bar/ToolBar.js
@@ -5,9 +5,21 @@ class ToolBar extends React.Component {
   render() {
     return (
       <div className="toolbar-container">
-        <button onClick={() => this.props.resetGame()}>Play Again?</button>
-        <button onClick={() => this.props.setDifficulty(3)}>Easy</button>
-        <button onClick={() => this.props.setDifficulty(6)}>Hard</button>
+        <button
+          className="ui button primary"
+          onClick={() => this.props.resetGame()}>
+          Play Again?
+        </button>
+        <button
+          onClick={() => this.props.setDifficulty(3)}
+          className="ui button positive">
+          Easy
+        </button>
+        <button
+          onClick={() => this.props.setDifficulty(6)}
+          className="ui button negative">
+          Hard
+        </button>
       </div>
     );
   }
